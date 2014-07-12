bcf063525fa13cac0fc9536cbf0a6e207d35635d
Trenton Large <trentonlarge@gmail.com>
2014-07-12 12:32:30 -0400

    Add better running mechanics

--- cron/cron.py
+++ cron/cron.py
@@ -57,7 +57,8 @@ class Job(object):
 class Scheduler(object):
 	def __init__(self):
 		self.jobs = []
-		self.thread = threading.Thread(target=self.run)
+		self.running = False
+		self.thread = None
 
 	def add(self, job):
 		self.jobs.append(job)
@@ -66,11 +67,23 @@ class Scheduler(object):
 		self.jobs.remove(job)
 
 	def start(self):
+		if self.is_running():
+			return
+
 		self.running = True
+		self.thread = threading.Thread(target=self.run)
 		self.thread.start()
 
 	def stop(self):
+		if not self.is_running():
+			return
+
 		self.running = False
+		self.thread.join()
+		self.thread = None
+
+	def is_running(self):
+		return self.thread and self.thread.is_alive()
 
 	def run(self):
 		while self.running:
